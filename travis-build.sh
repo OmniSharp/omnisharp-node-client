d4a21260f0bb8770d92ef9536f8887e4abcc22ed
David Driscoll <david.driscoll@gmail.com>
2015-07-01 11:04:59 -0400

    Updated build to download windows runtime

--- travis-build.sh
+++ travis-build.sh
@@ -8,6 +8,19 @@ cp -a approot/* .
 rm -rf approot
 popd
 
+curl -LO http://nuget.org/nuget.exe
+mono nuget.exe install dnx-clr-win-x86 -Prerelease -OutputDirectory roslyn/packages
+
+if [ ! -d "roslyn/packages/dnx-clr-win-x86.1.0.0-beta4" ]; then
+    echo 'ERROR: Can not find dnx-clr-win-x86.1.0.0-beta4 in output exiting!'
+    exit 1
+fi
+
+if [ ! -d "roslyn/packages/dnx-mono.1.0.0-beta4" ]; then
+    echo 'ERROR: Can not find dnx-mono.1.0.0-beta4 in output exiting!'
+    exit 1
+fi
+
 cp -f vendor/omnisharp.cmd.patch roslyn/omnisharp.cmd
 cp -f vendor/omnisharp.patch roslyn/omnisharp
 chmod +x roslyn/omnisharp
