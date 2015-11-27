62389c3954bfaadc6c95272dece4863ccbb2b277
David Driscoll <david.driscoll@gmail.com>
2015-11-27 14:56:54 -0500

    Fix build so that it can pass

--- build-server.sh
+++ build-server.sh
@@ -4,7 +4,12 @@ rm -rf roslyn
 mkdir -p roslyn
 pushd roslyn
 
+wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/$OMNISHARP_ROSLYN_VERSION/omnisharp.bootstrap.tar.gz
 wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/$OMNISHARP_ROSLYN_VERSION/omnisharp.tar.gz
+
+tar zxvf omnisharp.bootstrap.tar.gz
+rm -f omnisharp.bootstrap.tar.gz
+
 tar zxvf omnisharp.tar.gz
 rm -f omnisharp.tar.gz
 
@@ -15,4 +20,6 @@ popd
 
 cp -f vendor/omnisharp.cmd.patch roslyn/omnisharp.cmd
 cp -f vendor/omnisharp.patch roslyn/omnisharp
+rm roslyn/Bootstrapper
+rm roslyn/Bootstrapper.cmd
 chmod +x roslyn/omnisharp
