8fe41c9fc71163842ed067f330a3fb51f6ff18d1
David Driscoll <david.driscoll@gmail.com>
2016-02-20 23:35:45 -0500

    Removed checked in typings

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser\ambient\chai\chai.d.ts" />
-/// <reference path="browser\ambient\mocha\mocha.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
-/// <reference path="browser\definitions\lodash\lodash.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main\ambient\chai\chai.d.ts" />
-/// <reference path="main\ambient\mocha\mocha.d.ts" />
-/// <reference path="main\ambient\node\node.d.ts" />
-/// <reference path="main\definitions\lodash\lodash.d.ts" />
