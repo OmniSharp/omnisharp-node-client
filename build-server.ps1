011bc08f910dfab0da79eb01d4e6dd336b6bd8d6
David Driscoll <david.driscoll@gmail.com>
2016-06-01 21:32:21 -0400

    updated build for dotnet

--- build-server.ps1
+++ build-server.ps1
@@ -1,12 +1,5 @@
 $OMNISHARP_ROSLYN_VERSION=(Get-Content package.json | ConvertFrom-Json).'omnisharp-roslyn'
 
-pushd .
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))
-$Env:DNX_UNSTABLE_FEED='https://www.myget.org/F/aspnetcidev/api/v2'
-dnvm install 1.0.0-rc2-16388 -u
-dnvm use 1.0.0-rc2-16388
-popd
-
 Remove-Item vendor/omnisharp-roslyn -Recurse -Force
 mkdir vendor/omnisharp-roslyn
 pushd vendor/omnisharp-roslyn
@@ -17,10 +10,10 @@ Remove-Item source.tar.gz
 $dir = (gci . -Directory)[0].FullName;
 Copy-Item $dir\* . -Recurse
 Remove-Item $dir -Recurse
-dnu restore
+dotnet restore
 popd
 
 pushd vendor/src/OmniSharp.TypeScriptGeneration
-dnu restore
-dnx run ../../..
+dotnet restore
+dotnet run ../../..
 popd
