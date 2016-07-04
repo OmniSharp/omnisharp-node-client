505ec6b44f42e695f486f10e4cbc828eb0830cb5
David Driscoll <david.driscoll@gmail.com>
2016-07-04 14:00:33 -0400

    updated platform detection

--- lib/helpers/platform.ts
+++ lib/helpers/platform.ts
@@ -1,4 +1,4 @@
-import {execSync} from "child_process";
+import child_process from "child_process";
 
 export const supportedPlatformNames: { [index: string]: string; } = {
     get None() { return ""; },
@@ -16,32 +16,75 @@ export enum SupportedPlatform {
     OSX,
     CentOS,
     Debian,
+    Fedora,
+    OpenSUSE,
     RHEL,
-    Ubuntu
+    Ubuntu14,
+    Ubuntu16
 }
 
-export function getSupportedPlatform(platform: string) {
-    if (platform === "win32") {
+/* tslint:disable:quotemark */
+export function getSupportedPlatform(platform: string = process.platform) {
+    if (process.platform === 'win32') {
         return SupportedPlatform.Windows;
-    } else if (platform === "darwin") {
+    } else if (process.platform === 'darwin') {
         return SupportedPlatform.OSX;
-    } else if (platform === "linux") {
-        // Get the text of /etc/*-release to discover which Linux distribution we"re running on.
-        let release = execSync("cat /etc/*-release").toString().toLowerCase();
+    } else if (process.platform === 'linux') {
+        // Get the text of /etc/os-release to discover which Linux distribution we're running on.
+        // For details: https://www.freedesktop.org/software/systemd/man/os-release.html
+        const text = child_process.execSync('cat /etc/os-release').toString();
+        const lines = text.split('\n');
 
-        if (release.indexOf("ubuntu") >= 0) {
-            return SupportedPlatform.Ubuntu;
-        } else if (release.indexOf("centos") >= 0) {
-            return SupportedPlatform.CentOS;
-        } else if (release.indexOf("rhel") >= 0) {
-            return SupportedPlatform.RHEL;
-        } else if (release.indexOf("debian") >= 0) {
-            return SupportedPlatform.Debian;
+        function getValue(name: string) {
+            for (let line of lines) {
+                line = line.trim();
+                if (line.startsWith(name)) {
+                    const equalsIndex = line.indexOf('=');
+                    if (equalsIndex >= 0) {
+                        let value = line.substring(equalsIndex + 1);
+
+                        // Strip double quotes if necessary
+                        if (value.length > 1 && value.startsWith('"') && value.endsWith('"')) {
+                            value = value.substring(1, value.length - 1);
+                        }
+
+                        return value;
+                    }
+                }
+            }
+
+            return undefined;
+        }
+
+        const id = getValue("ID");
+
+        switch (id) {
+            case 'ubuntu':
+                const versionId = getValue("VERSION_ID");
+                if (versionId.startsWith("14")) {
+                    // This also works for Linux Mint
+                    return SupportedPlatform.Ubuntu14;
+                } else if (versionId.startsWith("16")) {
+                    return SupportedPlatform.Ubuntu16;
+                }
+            case 'centos':
+                return SupportedPlatform.CentOS;
+            case 'fedora':
+                return SupportedPlatform.Fedora;
+            case 'opensuse':
+                return SupportedPlatform.OpenSUSE;
+            case 'rehl':
+                return SupportedPlatform.RHEL;
+            case 'debian':
+                return SupportedPlatform.Debian;
+            case 'ol':
+                // Oracle Linux is binary compatible with CentOS
+                return SupportedPlatform.CentOS;
         }
     }
 
     return SupportedPlatform.None;
 }
-
+/* tslint:enable:quotemark */
 
 export const supportedPlatform = getSupportedPlatform(process.platform);
