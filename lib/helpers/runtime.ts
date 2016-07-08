fe44a1a61d1f1d264a7eb90af51c9b7db8c4ba9a
David Driscoll <david.driscoll@gmail.com>
2016-07-07 23:07:01 -0400

    removed spaces...

--- lib/helpers/runtime.ts
+++ lib/helpers/runtime.ts
@@ -114,7 +114,7 @@ export class RuntimeContext {
 
     private _getOsName() {
         if (this._platform === SupportedPlatform.Windows) return "win";
-        
+
         const name = SupportedPlatform[this._platform];
         if (name) return name.toLowerCase();
         return name;
